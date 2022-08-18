f1a1d3804d1f74ea86eca1f72cde4024410db881
uncu <uncu991@gmail.com>
2022-08-18 16:52:07 +0300

    static files upload

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-const fn = function () {
-    console.log("function working");
-}
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+
+app.use(express.static('public'))           // public isimli dosyadaki içerikler statik diye gösterdik
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"temp/index.html"))     
+})
+
+
+
+
+const port = 3000
+
+app.listen(port,()=>{
+    console.log(`Sunucu ${port}'unda başlatıldı`);
+})
+
